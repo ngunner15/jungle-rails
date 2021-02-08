487f94d4fbdbd24d945e41c06be4ab5109d4d2bd
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-08 13:44:58 -0500

    Add active_sale variable in index method

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,7 +1,9 @@
 class Admin::SalesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
     @sales = Sale.all
+    @active_sale = Sale.active.first
   end
 
 end
