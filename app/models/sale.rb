fe38e50beaba5e255b3847fe914b4a835d38528d
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-08 13:47:19 -0500

    Add a query to get active sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,16 @@
 class Sale < ActiveRecord::Base
 
+  validates :name, presence: true, length: { minimum: 3}
+  validates :percent_off, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+
+  # AR Scope
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?",
+    Date.current, Date.current)
+  end
+
   def finished?
     ends_on < Date.current
   end
